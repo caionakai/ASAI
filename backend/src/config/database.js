e08f6d444b207c65b7692e5d8c02ee26ec3171cc
Lucas Pereira Lopes <lkogici@gmail.com>
2020-04-18 04:50:54 +0100

    UPDATE: Changed some database properties connection

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -4,8 +4,10 @@ module.exports = {
     database: 'testeDatabase', //not final
     host: '127.0.0.1', //not final
     dialect: 'mysql',
-    logging: false,
-    operatorsAliases: false,
-    timezone: 'Europe/Lisbon', 
-    query: {raw: true}
+  //logging: false, //don't show in console the sql statement that is being executed
+  operatorsAliases: 0,
+  query: { raw: true },
+  define: {
+    timestamps: false //Don't use created_at and updated_at
   }
+}
