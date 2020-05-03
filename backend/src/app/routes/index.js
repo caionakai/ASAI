93b3d30840f29e3cffdf5ad36ade34def1128ce8
Lucas Pereira Lopes <lkogici@gmail.com>
2020-05-03 20:16:45 +0100

    UPDATE: Disabled the route for word
    - Don't expose the /world route
    - Removed the definition of World model with sequelize

--- backend/src/app/routes/index.js
+++ backend/src/app/routes/index.js
@@ -1,9 +1,7 @@
-  
 const express = require('express');
 const router = express.Router();
 
-const world = require('./worldRouter');
-
-router.use('/world', world);
+//const world = require('./worldRouter');
+//router.use('/world', world); //Example how to associate a file with a route
 
 module.exports = router;
