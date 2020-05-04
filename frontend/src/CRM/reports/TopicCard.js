972e9c9bf1169208ea669a2be8fb0fcfc82074ef
Eduardo Yuzo Nakai <yuzonakai@gmail.com>
2020-05-04 13:47:56 +0100

    Commit b4 Merge.

--- frontend/src/CRM/reports/TopicCard.js
+++ frontend/src/CRM/reports/TopicCard.js
@@ -2,7 +2,6 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 // comp. usado para diferentes tópicos: brand, sales, etc.
