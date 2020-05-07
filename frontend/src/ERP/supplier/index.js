2c64f5c040c272310fca2c2d5e4740f482995198
caio nakai <kaioesperto@hotmail.com>
2020-05-07 19:02:36 +0100

    update for supplier

--- frontend/src/ERP/supplier/index.js
+++ frontend/src/ERP/supplier/index.js
@@ -34,11 +34,8 @@ export default function Supplier() {
     email: "",
   });
 
-<<<<<<< HEAD
   const [supplierId, setSupplierId] = React.useState(null);
 
-=======
->>>>>>> supplier crud operation
   const handleChange = (evt) => {
     const value = evt.target.value;
     setSupplierInputs({ ...supplierInputs, [evt.target.name]: value });
