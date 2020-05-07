67b6b360982dda39fdd7227a67a268603209b207
willian <will.oliver1994@hotmail.com>
2020-05-07 22:46:50 +0100

    reports route
    created reports route

--- backend/src/app/routes/crmRouter.js
+++ backend/src/app/routes/crmRouter.js
@@ -4,4 +4,7 @@ const router = express.Router();
 //const world = require('./worldRouter');
 //router.use('/world', world); //Example how to associate a file with a route
 
+const reports= require('./reportsRouter');
+router.use('/reports', reports);
+
 module.exports = router;
