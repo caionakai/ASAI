275ecfed445d1b54e686be20df5d2a158f29bc16
txenta97 <61428053+txenta97@users.noreply.github.com>
2020-05-07 20:17:16 +0100

    add purchase

--- backend/src/app/routes/PurchaseRequestRouter.js
+++ backend/src/app/routes/PurchaseRequestRouter.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const supplierController = require("../controllers/supplierController");
+const PurchaseRequest = require("../controllers/PurchaseRequest");
 
 const isTimeout = (response) => { return response['error']['parent']['errorno'] == 'ETIMEDOUT' }
 
 const timeout = (res) => { return res.status(504).json({ successful: false, error: 'Timeout'}) }
-const undefinedError = (res) => { return res.status(400).json({ successful: false, error: `Fail to get all suppliers` }) }
+const undefinedError = (res) => { return res.status(400).json({ successful: false, error: `Fail to get all PurchaseRequest` }) }
 const error = (response, res) => { return isTimeout(response)? timeout(res) : undefinedError(res) }
 
 
 router.get('/', async (req, res) => {
-    const response = await supplierController.getAll();
+    const response = await PurchaseRequest.getAll();
 
     if(!response['successful']){
         return error(response, res)
@@ -22,7 +22,7 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
     const { id } = req.params;
 
-    const response = await supplierController.getById(id);
+    const response = await purchaseController.getById(id);
 
     if(!response['successful']){
         return error(response, res)
@@ -32,7 +32,7 @@ router.get('/:id', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-    const response = await supplierController.save(req.body);
+    const response = await PurchaseRequest.save(req.body);
 
     if(!response['successful']){
         return error(response, res)
@@ -44,7 +44,7 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
     const { id } = req.params;
 
-    const response = await supplierController.update(id, req.body);
+    const response = await PurchaseRequest.update(id, req.body);
 
     if(!response['successful']){
         return error(response, res)
@@ -56,7 +56,7 @@ router.put('/:id', async (req, res) => {
 router.delete('/:id', async (req, res) => {
     const { id } = req.params;
 
-    const response = await supplierController.delete(id);
+    const response = await PurchaseRequest.delete(id);
 
     if(!response['successful']){
         return error(response, res)
