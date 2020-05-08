f1528cbadc9bfbf11302a59d830ecb9a88e14b1d
Eduardo Yuzo Nakai <yuzonakai@gmail.com>
2020-05-08 18:16:08 +0100

    Final REPORTS module state.

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,18 +1,5 @@
-// module.exports = {
-//     username: 'root', //not final
-//     password: 'root', //not final
-//     database: 'testeDatabase', //not final
-//     host: '127.0.0.1', //not final
-//     dialect: 'mysql',
-//   //logging: false, //don't show in console the sql statement that is being executed
-//   operatorsAliases: 0,
-//   query: { raw: true },
-//   define: {
-//     timestamps: false //Don't use created_at and updated_at
-//   }
-// }
-
 module.exports = {
+<<<<<<< HEAD
 <<<<<<< HEAD
     username: 'fokinell', //not final
     password: 'ea6b4bb4', //not final
@@ -26,6 +13,13 @@ module.exports = {
   host: 'db4free.net', //not final
   dialect: 'mysql',
 >>>>>>> Still trying to Fix Stuff.
+=======
+    username: 'root', //not final
+    password: 'root', //not final
+    database: 'testeDatabase', //not final
+    host: '127.0.0.1', //not final
+    dialect: 'mysql',
+>>>>>>> Final REPORTS module state.
   //logging: false, //don't show in console the sql statement that is being executed
   operatorsAliases: 0,
   query: { raw: true },
