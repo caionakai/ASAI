0c039d113e4692287cbc9a1184f21b855805439d
Tiago Santos <tiagoffsantos2@hotmail.com>
2020-05-08 09:24:20 +0100

    correcção na route de Service Type

--- backend/src/app/routes/crmRouter.js
+++ backend/src/app/routes/crmRouter.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+const service_type = require('./serviceTypeRouter');
+router.use('/service_types', service_type);
+
 //const world = require('./worldRouter');
 //router.use('/world', world); //Example how to associate a file with a route
 
