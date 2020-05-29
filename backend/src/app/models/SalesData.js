d5790585d6f2ecb69a993e33354eccb44ed8601d
Giuseppe Setem <giuseppe7d@gmail.com>
2020-05-29 14:47:55 +0100

    fix sales data

--- backend/src/app/models/SalesData.js
+++ backend/src/app/models/SalesData.js
@@ -1,13 +1,17 @@
-const { Model , DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
 class SalesData extends Model {
   static init(sequelize) {
-    super.init({ details : DataTypes.STRING(100),}, {
-      sequelize,
-      tableName: 'SalesData'
-    });
-  }static associate(models) {
-    this.hasMany(models.Sale, { foreignKey: 'id', as: 'sale' });
+    super.init(
+      { details: DataTypes.STRING(100) },
+      {
+        sequelize,
+        tableName: "Salesdata",
+      }
+    );
+  }
+  static associate(models) {
+    this.hasMany(models.Sale, { foreignKey: "id", as: "sale" });
+  }
 }
-}
-module.exports =  SalesData                                                                                     ;
+module.exports = SalesData;
--- backend/src/app/routes/SalesDataRouter.js
+++ backend/src/app/routes/SalesDataRouter.js
@@ -1,68 +1,77 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const SalesDataController = require("../controllers/salesDataController");
 
-const isTimeout = (response) => { return response['error']['parent']['errorno'] == 'ETIMEDOUT' }
+const isTimeout = (response) => {
+  return response["error"]["parent"]["errorno"] == "ETIMEDOUT";
+};
 
-const timeout = (res) => { return res.status(504).json({ successful: false, error: 'Timeout'}) }
-const undefinedError = (res) => { return res.status(400).json({ successful: false, error: `Fail to get all Sales Data` }) }
-const error = (response, res) => { return isTimeout(response)? timeout(res) : undefinedError(res) }
+const timeout = (res) => {
+  return res.status(504).json({ successful: false, error: "Timeout" });
+};
+const undefinedError = (res) => {
+  return res
+    .status(400)
+    .json({ successful: false, error: `Fail to get all Sales Data` });
+};
+const error = (response, res) => {
+  return isTimeout(response) ? timeout(res) : undefinedError(res);
+};
 
+router.get("/", async (req, res) => {
+  const response = await SalesDataController.getAll();
 
-router.get('/', async (req, res) => {
-    const response = await SalesDataController.getAll();
+  if (!response["successful"]) {
+    return error(response, res);
+  }
 
-    if(!response['successful']){
-        return error(response, res)
-    }
-
-    return res.send(response);
+  return res.send(response);
 });
 
-router.get('/:id', async (req, res) => {
-    const { id } = req.params;
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
 
-    const response = await SalesDataController.getById(id);
+  const response = await SalesDataController.getById(id);
 
-    if(!response['successful']){
-        return error(response, res)
-    }
+  if (!response["successful"]) {
+    return error(response, res);
+  }
 
-    return res.send(response);
+  return res.send(response);
 });
 
-router.post('/', async (req, res) => {
-    const response = await SalesDataController.save(req.body);
+router.post("/", async (req, res) => {
+  const response = await SalesDataController.save(req.body);
 
-    if(!response['successful']){
-        return error(response, res)
-    }
+  if (!response["successful"]) {
+    return error(response, res);
+  }
 
-    return res.send(response);
+  return res.send(response);
 });
 
-router.put('/:id', async (req, res) => {
-    const { id } = req.params;
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
 
-    const response = await SalesDataController.update(id, req.body);
+  const response = await SalesDataController.update(id, req.body);
 
-    if(!response['successful']){
-        return error(response, res)
-    }
+  if (!response["successful"]) {
+    return error(response, res);
+  }
 
-    return res.send(response);
+  return res.send(response);
 });
 
-router.delete('/:id', async (req, res) => {
-    const { id } = req.params;
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
 
-    const response = await SalesDataController.delete(id);
+  const response = await SalesDataController.delete(id);
 
-    if(!response['successful']){
-        return error(response, res)
-    }
+  if (!response["successful"]) {
+    return error(response, res);
+  }
 
-    return res.send(response);
+  return res.send(response);
 });
 
 module.exports = router;
